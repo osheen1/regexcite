b5cdf3f916326f5b0d451cc7402cd2e155656e1f
osheen1 <osheen@jumpingrivers.com>
2022-09-07 11:17:26 +0100

    Updated prime function

--- R/prime.R
+++ R/prime.R
@@ -1,7 +1,24 @@
+#' Prime Test
+#'
+#' @param x An interger value
+#'
+#' @return A string
+#' @export
+#'
+#' @examples
+#' x = 7
+#' prime(7)
+#'
 p = 1
 prime <- function(x) {
   if (x == 1) {
     print("1 is not a prime number as it is only divisible by one number")
+  }
+  else if (x %% 1 != 0){
+    print("Only integers can be prime")
+  }
+  else if (x < 0){
+    print("Negative numbers cannot be prime")
   }
   else{
     for (i in seq(x)) {
